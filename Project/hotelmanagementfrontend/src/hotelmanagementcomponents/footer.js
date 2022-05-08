85c75107b22eb4e9d143c701da440cf4121d14d9
Neal69240 <rajindraakmeemana@gmail.com>
2022-05-08 23:30:49 +0530

    commit

---
+++ Project/hotelmanagementfrontend/src/hotelmanagementcomponents/footer.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import img7 from './images/room1.jpg';
+
+
+export default class footer extends Component{
+
+
+    
+   
+    
+    
+    
+    
+    
+
+
+render() {
+return (
+
+
+
+
+    <div class="footer">
+    <p>Created By ITPM_REG_WE_55</p>
+  </div>
+
+
+
+)
+}
+}
